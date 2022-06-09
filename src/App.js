debf23e0bfb12bddd41ca417afc04685d9ae9dfd
unknown <tom_ski18@yahoo.com>
2022-06-09 09:43:32 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-import React, { useState, useCallback } from 'react';
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
 
-import Button from './components/UI/Button/Button';
-import DemoOutput from './components/Demo/DemoOutput';
-import './App.css';
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
-
-  console.log('APP RUNNING');
-
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }
-  }, [allowToggle]);
-
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  };
+  const usersContext = {
+    users: DUMMY_USERS
+  }
 
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
