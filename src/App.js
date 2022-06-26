0ba72ad7ec3a50b996cdd10951003d5451a7bc44
unknown <tom_ski18@yahoo.com>
2022-06-26 19:17:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { useContext } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Ingredients from './components/Ingredients/Ingredients';
-import Auth from './components/Auth';
-import { AuthContext } from './context/auth-context';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import Layout from './components/layout/Layout';
 
-const App = props => {
-  const authContext = useContext(AuthContext);
-
-  let content = <Auth />;
-  if (authContext.isAuth) {
-    content = <Ingredients />;
-  }
-
-  return content;
-};
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import AuthContextProvider from './context/auth-context';
+import { FavoritesContextProvider } from './store/favorites-context';
 
 ReactDOM.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>,
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>,
   document.getElementById('root')
 );
